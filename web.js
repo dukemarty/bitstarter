34a45f7cf7c9465622b43cf9d83b207548cbab96
Martin Loesch <development@martinloesch.net>
2013-07-09 21:15:49 +0000

    Fixed typo.

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var respText = fs.readFileSyn("index.html").toString();
+    var respText = fs.readFileSync("index.html").toString();
     response.send(respText);
 });
 
